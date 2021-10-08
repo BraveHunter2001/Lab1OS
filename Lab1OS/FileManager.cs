3d75e7434281706de7f42705298a3f76b2f2443f
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2021-10-08 19:43:04 +0300

    add-create file

--- Lab1OS/FileManager.cs
+++ Lab1OS/FileManager.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Runtime.InteropServices;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using winapiFlags;
 
 
@@ -15,267 +10,55 @@ namespace Lab1OS
 		[DllImport("kernel32.dll")]
 		protected static extern uint GetLastError();
 
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool CopyFile([MarshalAs(UnmanagedType.LPUTF8Str)] string fromPathName,
-									[MarshalAs(UnmanagedType.LPUTF8Str)] string toPathName,
-									bool failIfExists);
-
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool MoveFile([MarshalAs(UnmanagedType.LPUTF8Str)] string fromPathName,
-									[MarshalAs(UnmanagedType.LPUTF8Str)] string toPathName);
-
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool MoveFileEx([MarshalAs(UnmanagedType.LPUTF8Str)] string fromPathName,
-									  [MarshalAs(UnmanagedType.LPUTF8Str)] string toPathName, uint flags);
-
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern IntPtr CreateFile([MarshalAs(UnmanagedType.LPUTF8Str)] string fileName,
-									  uint desiredAccess, uint shareMode, winapiFlags.SECURITY_ATTRIBUTES securityAttributes,
-									  uint creationDisposition, uint flagsAndAttributes, IntPtr templateFile);
-
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi, SetLastError = true)]
 		protected static extern bool CloseHandle(IntPtr hObject);
 
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern uint GetFileAttributes([MarshalAs(UnmanagedType.LPUTF8Str)] string fileName);
 
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool SetFileAttributes([MarshalAs(UnmanagedType.LPUTF8Str)] string fileName, uint fileAttributes);
+		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi,  CharSet = CharSet.Auto, SetLastError = true)]
+		protected static extern IntPtr CreateFile(string fileName,
+									  uint desiredAccess, uint shareMode, SECURITY_ATTRIBUTES securityAttributes,
+									  uint creationDisposition, uint flagsAndAttributes, IntPtr templateFile);
 
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool GetFileInformationByHandle(IntPtr fileHandle, BY_HANDLE_FILE_INFORMATION fileInfo);
+		
 
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool GetFileTime(IntPtr fileHandle, out FILETIME creationTime,
-									   out FILETIME lastAccessTime, out FILETIME lastWriteTime);
-
-		[DllImport("kernel32.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-		protected static extern bool SetFileTime(IntPtr fileHandle, in FILETIME creationTime,
-							   in FILETIME lastAccessTime, in FILETIME lastWriteTime);
-		protected bool GetFilePath(out string targetPath, bool allowInterrupt, string header = "Select target file full path name ")
-		{
-			Console.WriteLine(header + (allowInterrupt ?
-							  "(press empty to  abort):" : ":"));
-			targetPath = Console.ReadLine();
-			if (allowInterrupt && targetPath == "")
-				return false;
-			return true;
-		}
-
-		protected bool GetSourceTargetPaths(out string sourcePath, out string targetPath, bool allowInterrupt, bool checkSourceExists = true)
-		{
-			sourcePath = "";
-			targetPath = "";
-
-			do
-			{
-				if (!GetFilePath(out sourcePath, allowInterrupt, "Select source file path "))
-					return false;
-				if (!checkSourceExists || !File.Exists(sourcePath))
-				{
-					Console.WriteLine("Source file doesn't exist");
-					sourcePath = "";
-				}
-			} while (sourcePath == "");
-
-			if (!GetFilePath(out targetPath, allowInterrupt))
-			{
-				sourcePath = "";
-				return false;
-			}
-			return true;
-		}
-
-		public void CopyFile(bool checkOverwrite, IYNmessageBox YNmessageBox)
-		{
-			Console.WriteLine("Copy");
-			string source, target;
-			if (GetSourceTargetPaths(out source, out target, true))
-			{
-				if (CopyFile(source, target, checkOverwrite))
-					Console.WriteLine("File copied successfully");
-				else
-				{
-					uint lastErr = GetLastError();
-					if (!checkOverwrite)
-						Console.WriteLine($"File copy failed. Error code: {lastErr}");
-					else if (lastErr == 80 || lastErr == 183 && YNmessageBox.Show("File already exists. Overwrite?"))
-					{
-						if (CopyFile(source, target, false))
-							Console.WriteLine("File overwritten successfully");
-						else
-						{
-							lastErr = GetLastError();
-							Console.WriteLine($"File copy failed. Error code: {lastErr}");
-						}
-					}
-					else
-						Console.WriteLine($"File copy failed. Error code: {lastErr}");
-				}
-			}
-			else
-				Console.WriteLine("Operation aborted");
-		}
-
-
-		public void MoveFile(bool checkOverwrite, IYNmessageBox YNmessageBox)
-		{
-			Console.WriteLine("Move");
-			string source, target;
-			if (GetSourceTargetPaths(out source, out target, true))
-			{
-				if (checkOverwrite)
-				{
-					if (MoveFile(source, target))
-						Console.WriteLine("File moved successfully");
-					else
-					{
-						uint lastErr = GetLastError();
-						if (lastErr == 80 || lastErr == 183 && YNmessageBox.Show("File already exists. Overwrite?"))
-							if (MoveFileEx(source, target, (uint)MoveFlags.MOVEFILE_REPLACE_EXISTING
-								| (uint)MoveFlags.MOVEFILE_COPY_ALLOWED))
-								Console.WriteLine("File replaced successfully");
-							else
-							{
-								lastErr = GetLastError();
-								Console.WriteLine($"File replacement failed. Error code: {lastErr}");
-							}
-						else
-							Console.WriteLine($"File move failed. Error code: {lastErr}");
-					}
-				}
-				else if (MoveFile(source, target))
-					Console.WriteLine("File moved successfully");
-				else
-				{
-					uint lastErr = GetLastError();
-					Console.WriteLine($"File move failed. Error code: {lastErr}");
-				}
-			}
-			else
-				Console.WriteLine("Operation aborted");
-		}
+	
 
 		protected readonly static IntPtr INVALID_HANDLE_VALUE = (IntPtr)(-1);
-		public void CreateFile(bool allowInterrupt)
+		
+		private string GetFilePath()
 		{
-			string targetPath = "";
-			if (!GetFilePath(out targetPath, allowInterrupt))
+			Console.WriteLine("Please, input full file path:");
+			return Console.ReadLine();
+		}
+		public void CreateFile()
+		{
+			string fileName = "";
+
+			fileName = GetFilePath();
+
+			if (fileName == "")
 				return;
-			var handle = CreateFile(targetPath, (uint)DesiredAccess.GENERIC_WRITE | (uint)DesiredAccess.GENERIC_READ,
-						(uint)ShareMode.FILE_SHARE_READ | (uint)ShareMode.FILE_SHARE_WRITE | (uint)ShareMode.FILE_SHARE_DELETE,
-						null, (uint)CreationDisposition.CREATE_ALWAYS, (uint)winapiFlags.FileAttributes.FILE_ATTRIBUTE_NORMAL,
-						IntPtr.Zero);
+
+			uint desAcc = (uint)DesiredAccess.GENERIC_WRITE | (uint)DesiredAccess.GENERIC_READ;
+			uint shareMode = (uint)ShareMode.FILE_SHARE_READ | (uint)ShareMode.FILE_SHARE_WRITE | (uint)ShareMode.FILE_SHARE_DELETE;
+			 
+
+			var handle = CreateFile(fileName, desAcc,shareMode, null, (uint)CreationDisposition.CREATE_ALWAYS,
+				(uint)FileAttributes.FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
+
+
 			if (handle != INVALID_HANDLE_VALUE)
 			{
-				Console.WriteLine("File created successfully!");
+				Console.WriteLine($"this file {fileName} created successfully.");
 				CloseHandle(handle);
 			}
 			else
-				Console.WriteLine($"Error creating file. Error code: { GetLastError() }");
-		}
-
-		const uint INVALID_FILE_ATTRIBUTES = uint.MaxValue;
-		public void PrintFileAttributes(string targetPath)
-		{
-			uint attributes = GetFileAttributes(targetPath);
-			if (attributes != INVALID_FILE_ATTRIBUTES)
-				foreach (var f in Helper.ParseFlags<winapiFlags.FileAttributes>(attributes))
-					Console.WriteLine("\t-" + f);
-			else
-				Console.WriteLine($"Error occured. Error code {GetLastError()}");
-		}
-
-		public void SetFileAttributes(string targetPath, winapiFlags.FileAttributes setAttribute)
-		{
-			if (SetFileAttributes(targetPath, (uint)setAttribute))
-				Console.WriteLine("File attribute set successfully!");
-			else
-				Console.WriteLine($"Error setting file attribute. Error code {GetLastError()}");
-		}
-
-		bool GetFileReadHandle(string targetPath, out IntPtr handle)
-		{
-			handle = CreateFile(targetPath, (uint)DesiredAccess.GENERIC_READ,
-				(uint)ShareMode.FILE_SHARE_READ | (uint)ShareMode.FILE_SHARE_WRITE,
-				null, (uint)CreationDisposition.OPEN_EXISTING,
-				0, IntPtr.Zero);
-			if (handle == INVALID_HANDLE_VALUE)
 			{
-				Console.WriteLine($"Error occured opening file. Error code: {GetLastError()}");
-				return false;
+				Console.WriteLine($"[ERROR] Invalid creating file. Error code:{GetLastError()}");
 			}
-			return true;
 		}
 
-		bool GetFileWriteHandle(string targetPath, out IntPtr handle)
-		{
-			handle = CreateFile(targetPath, (uint)DesiredAccess.GENERIC_WRITE | (uint)DesiredAccess.GENERIC_READ,
-						(uint)ShareMode.FILE_SHARE_READ | (uint)ShareMode.FILE_SHARE_WRITE | (uint)ShareMode.FILE_SHARE_DELETE,
-						null, (uint)CreationDisposition.OPEN_EXISTING, (uint)winapiFlags.FileAttributes.FILE_ATTRIBUTE_NORMAL,
-						IntPtr.Zero);
-			if (handle == INVALID_HANDLE_VALUE)
-			{
-				Console.WriteLine($"Error occured opening file. Error code: {GetLastError()}");
-				return false;
-			}
-			return true;
-		}
-
-		public void PrintFileAttributesByHandle(string targetPath)
-		{
-			IntPtr handle;
-			if (!GetFileReadHandle(targetPath, out handle))
-				return;
-
-			BY_HANDLE_FILE_INFORMATION fileInfo = new BY_HANDLE_FILE_INFORMATION();
-
-			if (GetFileInformationByHandle(handle, fileInfo))
-			{
-				Console.WriteLine("==File attributes==");
-				foreach (var f in Helper.ParseFlags<winapiFlags.FileAttributes>(fileInfo.fileAttributes))
-					Console.WriteLine("\t-" + f);
-				Console.WriteLine($"Volume serial: {fileInfo.volumeSerialNumber}");
-				Console.WriteLine($"File links number: {fileInfo.numberOfLinks}");
-				Console.WriteLine($"File-size high id: {fileInfo.fileSizeHigh}");
-				Console.WriteLine($"File-size low id: {fileInfo.fileSizeLow}");
-				Console.WriteLine($"File index high id: {fileInfo.fileIndexHigh}");
-				Console.WriteLine($"File index low id: {fileInfo.fileIndexLow}");
-				
-			}
-			else
-				Console.WriteLine($"Error  reading file attributes. Error code: {GetLastError()}");
-			CloseHandle(handle);
-		}
-
-		public void PrintFileTimeAttributes(string targetPath)
-		{
-			IntPtr handle;
-			if (!GetFileReadHandle(targetPath, out handle))
-				return;
-			FILETIME creationTime, accessTime, writeTime;
-			if (GetFileTime(handle, out creationTime, out accessTime, out writeTime))
-			{
-				Console.WriteLine($"File creation time: {creationTime.FileTimeToString()}");
-				Console.WriteLine($"Last file access time: {accessTime.FileTimeToString()}");
-				Console.WriteLine($"Last file write time: {writeTime.FileTimeToString()}");
-			}
-			else
-				Console.WriteLine($"Error reading file time attributes. Error code: {GetLastError()}");
-			CloseHandle(handle);
-		}
-
-		public void SetFileTimeAttributes(string targetPath)
-		{
-			IntPtr handle;
-			if (!GetFileWriteHandle(targetPath, out handle))
-				return;
-			var t = Helper.GetSystemFileTime();
-			if (SetFileTime(handle, in t, in t, in t))
-				Console.WriteLine("File time attributes set successfully");
-			else
-				Console.WriteLine($"Error  writing file time attributes. Error code: {GetLastError()}");
-			CloseHandle(handle);
-		}
+	
+	
 	}
 }
--- Lab1OS/Program.cs
+++ Lab1OS/Program.cs
@@ -12,88 +12,14 @@ namespace Lab1OS
 		static DriveInfo driveManager = new DriveInfo();
 		static DirectoryManager directoryManager = new DirectoryManager();
 		static FileManager fileManager = new FileManager();
-		static OverlappedFileCopier overlappedFileCopier = new OverlappedFileCopier();
-		static IYNmessageBox YNmessageBox = new YNMessageBox();
+		
 
-		static FlexMenu driveFlexMenu = new FlexMenu("Get hard drive details", new IMenuItem[] { });
-
-		static MenuWithDataRequest<String> fileAttributeManagementMenu =
-				new MenuWithDataRequest<String>("File attribute management",
-					new IMenuItem[]{
-						new MenuItem("Get file attributes",
-							()=>fileManager.PrintFileAttributes(fileAttributeManagementMenu.Data)),
-						new MenuItem("Get file attributes by handle",
-							()=>fileManager.PrintFileAttributesByHandle(fileAttributeManagementMenu.Data)),
-						new MenuItem("Get file time attributes",
-							()=>fileManager.PrintFileTimeAttributes(fileAttributeManagementMenu.Data)),
-						new MenuItem("Set file time attributes",
-							()=>fileManager.SetFileTimeAttributes(fileAttributeManagementMenu.Data)),
-						new MenuWithDataRequest<String>("Set file attributes",
-							CreateSetAttributesMenu(), ()=>fileAttributeManagementMenu.Data)
-					},
-					() => SelectFile());
-
-		static IMenu menu = new Menu("Main Menu", new IMenuItem[]
-			{
-				new Menu("Hard drive info", new IMenuItem[]
-					{
-						new MenuItem("Get all hard drives list", driveManager.PrintAllDrives),
-						driveFlexMenu
-					}).AddOnFirstSelectAction(CreateDriveNamedMenus), //add drives to menu
-				new Menu("Directory management", new IMenuItem[]
-					{
-						new MenuItem("Create directory", directoryManager.CreateDirectory),
-						new MenuItem("Remove directory", directoryManager.RemoveDirectory)
-					}),
-				new Menu("File management", new IMenuItem[]
-					{
-						new MenuItem("Copy file", () => fileManager.CopyFile(true, YNmessageBox)),
-						new MenuItem("Copy file overlapped", overlappedFileCopier.Copy),
-						new MenuItem("Move file", () => fileManager.MoveFile(true, YNmessageBox)),
-						new MenuItem("Create file", () => fileManager.CreateFile(true)),
-						fileAttributeManagementMenu
-					})
-			});
-
-		static string SelectFile(bool checkFileExistance = false)
-		{
-			var result = "";
-			do
-			{
-				Console.WriteLine("Input file full path");
-				result = Console.ReadLine();
-				if (checkFileExistance && !File.Exists(result))
-					Console.WriteLine("Incorrect path or file");
-			} while (checkFileExistance && !File.Exists(result));
-			return result;
-		}
-		static IMenuItem[] CreateSetAttributesMenu()
-		{
-			winapiFlags.FileAttributes[] values = (winapiFlags.FileAttributes[])Enum.GetValues(typeof(winapiFlags.FileAttributes));
-			var res = new IMenuItem[values.Length];
-			for (int i = 0; i < values.Length; i++)
-			{
-				int j = i; 
-				res[i] = new MenuItem(values[i].ToString(), () => fileManager.SetFileAttributes(
-					fileAttributeManagementMenu.Data, values[j]));
-			}
-			return res;
-		}
-		static void CreateDriveNamedMenus()
-		{
-			var drives = driveManager.GetAllDrives();
-			IMenuItem[] menuItems = new IMenuItem[drives.Count];
-			for (int i = 0; i < drives.Count; i++)
-			{
-				int j = i; 
-				menuItems[i] = new MenuItem("Drive " + drives[i], () => driveManager.PrintFullInfo(drives[j]));
-			}
-			driveFlexMenu.AddItems(menuItems);
-		}
-
+		
 		static void Main(string[] args)
 		{
-			menu.Select();
+
+			fileManager.CreateFile();
+			Console.ReadKey();
 		}
 	}
 }
